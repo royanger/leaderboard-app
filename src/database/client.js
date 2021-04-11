2a7df88fecf4d9ef1bc45195e32fb65979f0862b
Roy Anger <roy@royanger.com>
2021-04-11 11:03:57 -0400

    Definitely was overthinking the whole .env stuff

--- src/database/client.js
+++ src/database/client.js
@@ -1,16 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const key =
-   process.env.NODE_ENV === 'development'
-      ? process.env.REACT_APP_FAUNA_ITEMEDITOR_KEY
-      : process.env.REACT_APP_FAUNA_ITEMEDITOR_KEY
-
-console.log('FAUNA KEY', process.env.FAUNA_ITEMEDITOR_KEY)
-
 export const client = new ApolloClient({
    uri: 'https://graphql.fauna.com/graphql',
    headers: {
-      authorization: `Bearer ${key}`,
+      authorization: `Bearer ${process.env.REACT_APP_FAUNA_ITEMEDITOR_KEY}`,
    },
    cache: new InMemoryCache(),
 })
