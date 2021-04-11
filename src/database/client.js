bb908d0c2c865d1fe94693d2e99ee15c45885d23
Roy Anger <roy@royanger.com>
2021-04-11 00:30:12 -0400

    Removed 2 unused vars to test build on Vercel w/ Firebase and Fauna

--- src/database/client.js
+++ src/database/client.js
@@ -1,18 +1,4 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-
-// const httpLink = createHttpLink({
-//    uri: 'https://graphql.fauna.com/graphql',
-// })
-
-// const authLink = setContext((_, { headers }) => {
-//    return {
-//       headers: {
-//          ...headers,
-//          authorization: `Bearer ${process.env.REACT_APP_FAUNA_ITEMEDITOR_KEY}`,
-//       }
-//    }
-// })
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
    uri: 'https://graphql.fauna.com/graphql',
