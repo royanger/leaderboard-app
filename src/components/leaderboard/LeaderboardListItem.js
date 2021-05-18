c90e43bc0f51e7b2025c03b608122fd48038bc81
Roy Anger <roy@royanger.com>
2021-05-17 20:52:21 -0400

    Changed X icon to button for delete.

--- src/components/leaderboard/LeaderboardListItem.js
+++ src/components/leaderboard/LeaderboardListItem.js
@@ -7,24 +7,8 @@ function LeaderboardListItem({ board, onClick, index }) {
             <strong>{board.title}</strong>
          </div>
          <div>{board.description}</div>
-         <div className="controls" onClick={() => onClick(board._id)}>
-            <svg
-               xmlns="http://www.w3.org/2000/svg"
-               style={{
-                  height: '1.5rem',
-                  width: 'auto',
-               }}
-               fill="none"
-               viewBox="0 0 24 24"
-               stroke="currentColor"
-            >
-               <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-               />
-            </svg>
+         <div className="controls">
+            <button onClick={() => onClick(board._id)}>Delete</button>
          </div>
       </div>
    )
