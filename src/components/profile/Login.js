0cee40af778c521e67b18924813ed80b41b44162
Roy Anger <roy@royanger.com>
2021-05-17 20:27:33 -0400

    Removed bootstrap from login page

--- src/components/profile/Login.js
+++ src/components/profile/Login.js
@@ -2,24 +2,17 @@ import * as React from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import { Container } from 'react-bootstrap'
 
 function Login({ uiConfig, firebaseAuth }) {
    return (
       <>
-         <Container
-            style={{
-               marginTop: '120px',
-            }}
-         >
-            <h2 style={{ textAlign: 'center', marginBottom: '40px' }}>
-               Sign In/Register
-            </h2>
+         <div className="login">
+            <h1>Sign In/Register</h1>
             <StyledFirebaseAuth
                uiConfig={uiConfig}
                firebaseAuth={firebase.auth()}
             />
-         </Container>
+         </div>
       </>
    )
 }
