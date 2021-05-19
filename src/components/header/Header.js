07fb969fcf85aaa0cd100b781665badb7631fa94
Roy Anger <roy@royanger.com>
2021-05-19 15:15:45 -0400

    Refactored header/nav to simplify

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,6 @@ function Header({ name, isSignedIn, handleLogout }) {
       setShowDropdown(!showDropdown)
    }
 
-   const clearDropdown = e => {
-      setShowDropdown(false)
-   }
-
    const logout = e => {
       setShowDropdown(false)
       handleLogout()
@@ -20,36 +16,26 @@ function Header({ name, isSignedIn, handleLogout }) {
    return (
       <header className="header">
          <div className="wrapper">
-            <Link to="/" onClick={clearDropdown}>
+            <Link to="/">
                <h1>{process.env.REACT_APP_SITE_TITLE}</h1>
             </Link>
             <nav>
                <div>
                   <ul>
-                     <li>
-                        <Link to="/" onClick={clearDropdown}>
-                           Leaderboards
-                        </Link>
-                     </li>
                      {isSignedIn ? (
-                        <div className="dropdown">
+                        <>
                            <li>
-                              <button onClick={handleDropdown}>
-                                 {name.displayName}
-                              </button>
-                           </li>
-                           <div
-                              className={`dropdown-content ${
-                                 showDropdown ? 'show' : 'hide'
-                              }
-                              `}
-                           >
-                              <Link to="/profile" onClick={clearDropdown}>
-                                 Profile
+                              <Link to="/profile">
+                                 <button>
+                                    {/* {name.displayName} */}
+                                    Profile / Add Leaderboard
+                                 </button>
                               </Link>
+                           </li>
+                           <li>
                               <button onClick={logout}>Logout</button>
-                           </div>
-                        </div>
+                           </li>
+                        </>
                      ) : (
                         ''
                      )}
